f46f9764e66c0d88265acc5ee8afdcfca0a3284d
marijavelickovska <marija_velickovska88@hotmail.com>
2025-06-25 10:13:19 +0200

    Create Django project with app, basic view and URL routing

---
+++ grow_healthy/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for grow_healthy project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grow_healthy.settings')
+
+application = get_asgi_application()
