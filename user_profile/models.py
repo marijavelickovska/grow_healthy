5e75394012f5211b5df084b40a4811f1dc5ce5a7
marijavelickovska <marija_velickovska88@hotmail.com>
2025-06-27 12:27:48 +0200

    Add Profile model with user, name, image and about fields

--- user_profile/models.py
+++ user_profile/models.py
@@ -2,4 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile'
+    )
+    name = models.CharField(max_length=100, verbose_name="Your name")
+    image = CloudinaryField('image', default='placeholder', blank=True)
+    about = models.TextField(
+        max_length=500, blank=True, verbose_name="Something about you"
+    )
