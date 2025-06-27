0feefbfc389ff05769865b33d509839b182a5533
marijavelickovska <marija_velickovska88@hotmail.com>
2025-06-27 15:49:22 +0200

    Register Recipe model in Django admin and add custom display

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Meal
+from .models import Meal, Recipe
 
 
 @admin.register(Meal)
+@admin.register(Recipe)
 class MealAdmin(admin.ModelAdmin):
     """
     Admin configuration for the Meal model.
@@ -12,3 +13,17 @@ class MealAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
     list_filter = ('name',)
+
+
+class RecipeAdmin(SummernoteModelAdmin):
+    """
+    Admin configuration for the Recipe model.
+    Displays recipe info, allows search and filtering,
+    prepopulated fields and rich-text editor.
+    """
+
+    list_display = ('title', 'author', 'created_on')
+    search_fields = ['title', 'author__username']
+    list_filter = ('meal_type', 'created_on',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('ingredients', 'instructions')
