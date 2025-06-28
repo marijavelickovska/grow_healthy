1c16edda5ecd47fd40c2627efecfef00e8096238
marijavelickovska <marija_velickovska88@hotmail.com>
2025-06-28 14:34:47 +0200

    Register Like model in admin and add custom display

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Meal, Recipe, Favourite
+from .models import Meal, Recipe, Favourite, Like
 
 
 @admin.register(Meal)
@@ -38,3 +38,14 @@ class FavouriteAdmin(admin.ModelAdmin):
     list_display = ('user', 'recipe', 'added_at')
     search_fields = ['user__username', 'recipe__title']
     list_filter = ('added_at',)
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Like model.
+    Displays like info and allows search and filtering.
+    """
+    list_display = ('user', 'recipe', 'created_on')
+    search_fields = ['user__username', 'recipe__title']
+    list_filter = ('created_on',)
