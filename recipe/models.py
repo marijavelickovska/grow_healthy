b457bf96c317742ab10a9612bbcb2c815ea720c2
marijavelickovska <marija_velickovska88@hotmail.com>
2025-06-28 13:29:56 +0200

    Create Favourite model

--- recipe/models.py
+++ recipe/models.py
@@ -28,3 +28,19 @@ class Recipe(models.Model):
 
     def __str__(self):
         return f"{self.title} | by {self.author}"
+
+
+class Favourite(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='favourites'
+    )
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='favourited_by'
+    )
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'recipe')  
+
+    def __str__(self):
+        return f"{self.user.username} | {self.recipe.title}"
