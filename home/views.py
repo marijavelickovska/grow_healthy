0b4f04eeeb402803260e19acf7522db46dde6977
marijavelickovska <marija_velickovska88@hotmail.com>
2025-06-30 19:00:10 +0200

    Add Home view to return all recipes

--- home/views.py
+++ home/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.views import generic
+from recipe.models import Recipe
 
 
-def home(request):
-    return render(request, 'home/home.html')
+class Home(generic.ListView):
+    """
+    Returns all recipes in :model:`recipe.Recipe`
+    and displays them in a page of eight posts.
+    **Context**
+
+    ``queryset``
+        All instances of :model:`recipe.Recipe`
+    ``paginate_by``
+        Number of posts per page.
+
+    **Template:**
+
+    :template:`home/home.html`
+    """
+
+    queryset = Recipe.objects.all()
+    template_name = "home/home.html"
+    paginate_by = 8
+    context_object_name = 'recipe_list'
