60b9797f977e56eeac0cf04c831ef4a5e430d13b
marijavelickovska <marija_velickovska88@hotmail.com>
2025-07-01 15:39:11 +0200

    Add templates for my_recipe, favourites, add_recipe and update URLs

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('my_recipes/', views.dashboard, name='my_recipes'),
+    path('favourites/', views.dashboard, name='favourites'),
+    path('add_recipe/', views.dashboard, name='add_recipe'),
 ]
