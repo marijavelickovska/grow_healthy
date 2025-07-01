4c7f8d0161ae89c3dbaf1b3491749edd419f8158
marijavelickovska <marija_velickovska88@hotmail.com>
2025-07-01 12:11:09 +0200

    Add click handler on recipe cards to show login/signup message

---
+++ static/js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let cards = document.getElementsByClassName("card-recipes-home");
+
+    for (let card of cards) {
+        card.addEventListener("click", showRecipeMessage);
+    }
+
+});
+
+
+function showRecipeMessage() {
+    let messageDiv = document.getElementById("recipe-message");
+    let joinSection = document.getElementById("join_us");
+
+    messageDiv.style.display = "block";
+
+    setTimeout(() => {
+        messageDiv.style.display = "none";
+    }, 5000);
+
+    if (joinSection) {
+        joinSection.scrollIntoView({
+            behavior: "smooth"
+        });
+    }
+}
