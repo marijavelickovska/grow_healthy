c48a2b104bfa93d09192137e814beb2bc9c13ab4
marijavelickovska <marija_velickovska88@hotmail.com>
2025-07-05 14:55:42 +0200

    Add JavaScript logic to handle comment editing

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,9 @@
+const editButtons = document.getElementsByClassName("btn-edit");
+const commentText = document.getElementById("id_body"); 
+const commentForm = document.getElementById("commentForm");
+const submitButton = document.getElementById("submitButton");
+
+
 // On DOMContentLoaded: Add event listener to cards to show a message when clicked
 document.addEventListener("DOMContentLoaded", function () {
    let cards = document.getElementsByClassName("card-home");
@@ -25,6 +31,18 @@ function showRecipeMessage() {
             behavior: "smooth"
         });
     }
+}
+
+
+for (let button of editButtons) {
+  button.addEventListener("click", (e) => {
+    let commentId = e.currentTarget.getAttribute("data-comment_id");
+    let recipeId = e.currentTarget.getAttribute("data-recipe_id");
+    let commentContent = document.getElementById(`comment${commentId}`).innerText;
+    commentText.value = commentContent;
+    submitButton.innerText = "Update";
+    commentForm.setAttribute("action", `/user_profile/recipe/${recipeId}/edit_comment/${commentId}/`);
+  });
 }
 
 
